d8be4b663a8f291d09d12c7cce612a42584a79eb
Miguel Alejandro <migtorruco@gmail.com>
2023-10-08 00:50:00 -0600

    tests

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const opiniones = ["mierda", "basura", "espagueti"]
 
 for (let opinion of opiniones) console.log(opinion)
 
+if (opiniones.length == 3) {
+    console.log("Vaya cantidad de opiniones malas que tienes sobre JS, ¿hm?")
+}
+
 console.log("Buenas tardes")
---
+++ test.js
@@ -0,0 +1,2 @@
+console.log("Bueno, ojalá supiera cómo escribir tests en JavaScript")
+console.log("de todas formas, aprovecho para tirarle hate")
